151d8a4aeda06bb3ec521f00de52b001f83ee593
hood5599-commits <hood.5599@gmail.com>
2026-08-09 00:26:48 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -373,7 +373,7 @@ export default function App() {
                       color: '#64748b',
                       display: 'flex',
                       alignItems: 'center',
-                      justify: 'center'
+                      justifyContent: 'center'
                     }}
                   >
                     ✖
